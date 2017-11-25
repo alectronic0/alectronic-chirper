8cfc749752c28f952174228d2034858983a0d5c7
Alec Doran-Twyford <alec@alectronic.co>
2017-11-25 16:27:16 +0000

    working on adding codecov

---
+++ test/app-test.spec.js
@@ -0,0 +1,11 @@
+describe("alectronic-chipper", function(){
+  beforeEach(function(){
+  });
+
+  after(function(){
+  });
+
+//  it('a = a', function() {
+//    expect("a").to.eql("a")
+//  })
+});
